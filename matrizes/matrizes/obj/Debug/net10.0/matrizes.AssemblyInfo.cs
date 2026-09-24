4f6801c8275af10185b1e1618066bc2f5af6debb
bruno-falchetti <falchettibruno6@gmail.com>
2026-09-23 21:59:46 -0300

    save

--- matrizes/matrizes/obj/Debug/net10.0/matrizes.AssemblyInfo.cs
+++ matrizes/matrizes/obj/Debug/net10.0/matrizes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("matrizes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9997755e30763610007054b5aca6db9c7433e7dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71a9d4c0c1b20f50ee33d7fb483b09ab2fcf43e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("matrizes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("matrizes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
